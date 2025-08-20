7b73408502d2f4fc49982c9de7c43868342f2fd6
astro-jets <mphatsomtogolo30@gmail.com>
2025-08-20 14:24:38 +0200

    Added topics from database

--- components/forms/LessonDetailsModal.tsx
+++ components/forms/LessonDetailsModal.tsx
@@ -19,8 +19,6 @@ export const LessonDetailsModal = ({ topics }: { topics: { topic_id: string; top
         }
     };
 
-
-
     const [isRecording, setIsRecording] = useState(false);
     const mediaRecorderRef = useRef<MediaRecorder | null>(null);
     const recordedChunksRef = useRef<Blob[]>([]);
@@ -146,18 +144,36 @@ export const LessonDetailsModal = ({ topics }: { topics: { topic_id: string; top
                             </select>
                         </div>
 
-                        {/* Notes Textarea */}
-                        <div className="relative">
-                            <label className="block mb-1 font-semibold text-gray-700">Lesson Summary & Observations</label>
+                        <div className="flex space-x-6">
+                            {/* Notes Textarea */}
+                            <div className="relative w-[65%]">
+                                <label className="block mb-1 font-semibold text-gray-700">Lesson Summary & Observations</label>
 
-                            <textarea
-                                placeholder="Lesson Summary & Observations "
-                                className="w-full px-4 pt-6 pb-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-purple-500 focus:outline-none peer"
-                                rows={5}
-                                // value={notes}
-                                // onChange={(e) => setNotes(e.target.value)}
-                                required
-                            />
+                                <textarea
+                                    placeholder="Lesson Summary & Observations "
+                                    className="w-full px-4 pt-6 pb-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-purple-500 focus:outline-none peer"
+                                    rows={5}
+                                    // value={notes}
+                                    // onChange={(e) => setNotes(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            {/* Upload Recording */}
+                            <div className='w-[30%] flex flex-col items-center'>
+                                <label className="block mb-1 font-semibold text-gray-700">
+                                    Upload Lesson Recording
+                                </label>
+                                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100">
+                                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                        </svg>
+                                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 text-center"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                        <p className="text-xs text-gray-500 dark:text-gray-400">  MP4 (MAX. 50mb)</p>
+                                    </div>
+                                    <input id="dropzone-file" type="file" className="hidden" onChange={(e) => { console.log(e) }} />
+                                </label>
+                            </div>
                         </div>
 
                         {/* Homework Button */}
@@ -170,23 +186,6 @@ export const LessonDetailsModal = ({ topics }: { topics: { topic_id: string; top
                             >
                                 ➕ Add Homework
                             </button>
-                        </div> */}
-
-                        {/* Upload Recording */}
-                        {/* <div>
-                            <label className="block mb-1 font-semibold text-gray-700">
-                                Upload Google Meet Recording
-                            </label>
-                            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100">
-                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                    </svg>
-                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                    <p className="text-xs text-gray-500 dark:text-gray-400">  MP4 (MAX. 50mb)</p>
-                                </div>
-                                <input id="dropzone-file" type="file" className="hidden" onChange={(e) => { console.log(e) }} />
-                            </label>
                         </div> */}
 
                         {/* Additional Notes */}
